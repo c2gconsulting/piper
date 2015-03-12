e9b486cec8c3492e1414bb27855327facc3685e8
Kachi Izukanne <cizukanne@c2gconsulting.com>
2015-03-12 19:28:04 +0100

    Build central server and virtual host framework

--- generic/lib/cache.js
+++
@@ -1,6 +0,0 @@
-exports.getRedisClient = function(){
-    var redis = require("redis");
-    var redisConfig = require("../config/redis.json");
-    
-    return redis.createClient(redisConfig.port, redisConfig.host, {});
-}
